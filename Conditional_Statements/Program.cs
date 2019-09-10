38f608468fdadafbad011c92da7c0cc82c64f12f
Shreya Gadikar <sggadikar@usf.edu>
2019-09-10 19:48:48 -0400

    Add project files.

---
+++ Conditional_Statements/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Conditional_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years of experience do you have in professional programming?");
+            try
+            {
+                string exp = Console.ReadLine();
+
+                switch(int.Parse(exp))
+                {
+                    case 0:
+                        Console.WriteLine("Need lots of practice!");
+                        break;
+                    case 1:
+                        Console.WriteLine("Looks like you have a little experience!");
+                        break;
+                    case 2:
+                        Console.WriteLine("Wow! You've been doing this for a little while!");
+                        break;
+                    default:
+                        Console.WriteLine("You are an expert!");
+                        break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an integer value.");
+            }
+        }
+    }
+}
